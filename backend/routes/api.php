03c4a5c3f5475eb837876881cc8ba11ec0ed3ebd
JHugoSilva <hugosilvahs31@gmail.com>
2025-08-03 17:53:06 -0300

    Adicionando uma rota fake para teste de APi

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -7,10 +7,14 @@ use App\Http\Controllers\MovieController;
 
 Route::get('/movies', [MovieController::class, 'index']);
 Route::get('/movies/search', [MovieController::class, 'searchTitle']);
-// Route::get('/movies/{id}', [MovieController::class, 'getDetails'])->name('movies.show');
 
 Route::get('/favorites', [FavoriteMovieController::class, 'index']);
 Route::get('/favorites/genres', [FavoriteMovieController::class, 'getGenres']);
 Route::post('/favorites', [FavoriteMovieController::class, 'store']);
-Route::delete('/favorites/{tmdb_id}', [FavoriteMovieController::class, 'destroy']);
+Route::delete('/favorites/{id}', [FavoriteMovieController::class, 'destroy']);
+
+//Rota para testar a API
+Route::get('/ping', function(){
+    return 'pong';
+});
 
